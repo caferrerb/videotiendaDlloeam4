ae52c02583faae32f1d07be32a6641ac4643feb5
laura posada <laura posada@DESKTOP-F91MVUQ>
2016-10-27 14:52:15 -0500

    controlador gestionar venta

---
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorGestionarVenta.java
@@ -0,0 +1,36 @@
+package co.edu.eam.ingesoft.videotienda.vista.controladores;
+
+
+
+import java.awt.TextField;
+
+import javax.swing.text.TableView;
+
+import org.hibernate.hql.internal.ast.tree.InitializeableNode;
+import org.springframework.stereotype.Controller;
+
+import co.edu.eam.ingesoft.videotienda.vista.util.BaseController;
+import javafx.fxml.FXML;
+
+@Controller
+public class ControladorGestionarVenta extends BaseController implements InitializeableNode {
+
+	@FXML
+	private TextField jtfTitulo;
+	
+	@FXML
+	private TableView jttablacontenidoPelicula;
+	
+	
+	@Override
+	public void initialize(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	public void buscar(){
+		
+	}
+
+}
