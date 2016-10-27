10425d16529241ef6809ea4938dd45777c86ed1c
GAR-T <GAR-T@GART>
2016-10-26 22:11:39 -0500

    ss

--- videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorGestionarActores.java
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorGestionarActores.java
@@ -47,6 +47,6 @@ public class ControladorGestionarActores extends BaseController implements Initi
 	}
 	
 	public void crear(){
-		System.out.println("Creandooo...");
+		System.out.println("Creandoooo...");
 	}
 }
