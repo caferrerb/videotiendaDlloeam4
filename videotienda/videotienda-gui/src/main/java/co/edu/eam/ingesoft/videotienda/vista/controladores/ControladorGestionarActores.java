78dc27bb3c6912de76cbdb7e8946626276c27a04
GAR-T <GAR-T@GART>
2016-10-26 20:10:44 -0500

    Act...

---
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorGestionarActores.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package co.edu.eam.ingesoft.videotienda.vista.controladores;
+
+import java.awt.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.hibernate.event.spi.InitializeCollectionEventListener;
+import org.springframework.stereotype.Controller;
+
+import co.edu.eam.ingesoft.videotienda.vista.util.BaseController;
+
+/**
+ * @author GAR-T
+ *
+ */
+@Controller
+public class ControladorGestionarActores extends BaseController {
+
+	@FXML
+	private TextField tfDocumento;
+	
+	@FXML
+	private TextField tfDocumento;
+	
+	public void initializable(URL location, ResourceBundle resources){
+		
+	}
+}
