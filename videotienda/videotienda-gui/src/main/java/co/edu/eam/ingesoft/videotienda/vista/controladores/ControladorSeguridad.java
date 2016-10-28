2d2284afda35f4cd716cfbe032a03657f64c3fa5
lenovo <lenovo@LAPTOP-DJSHJFAD>
2016-10-28 08:47:17 -0500

    commit

--- videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorSeguridad.java
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorSeguridad.java
@@ -1,8 +1,45 @@
 package co.edu.eam.ingesoft.videotienda.vista.controladores;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+
+import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Rol;
+import co.edu.eam.ingesoft.videotienda.vista.util.BaseController;
+import co.edu.eam.ingesoft.videotienda.vista.util.TipoNotificacion;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
-public class ControladorSeguridad {
+@Controller
+public class ControladorSeguridad extends BaseController implements Initializable {
 
+	@FXML
 	private TextField tfNombreRol;
+		
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+
+	}
+
+	@FXML
+	public void crear() {
+
+		Rol rol = new Rol();
+		rol.setDescripcion(tfNombreRol.getText());
+		rol.setDescripcion(tfNombreRol.getText());
+
+
+//		boCiudad.crear(ciudad);
+//		notificar("Gestionar Ciudad", "Ciudad creada con exito!!!", TipoNotificacion.INFO);
+	}
+	
+	@FXML
+	public void llenarTabla(){
+		
+		
+	}
+	
 }
---
+++ videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BORol.java
@@ -0,0 +1,13 @@
+package co.edu.eam.ingesoft.videotienda.logica.bos;
+
+import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Rol;
+
+public class BORol extends BOGenerico<Rol>{
+
+	@Override
+	public void crear(Rol entidad) {
+		// TODO Auto-generated method stub
+		super.crear(entidad);
+	}
+	
+}
