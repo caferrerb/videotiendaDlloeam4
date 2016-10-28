4bb2c2560a09c31ea7093b9a827b75541c278b19
Sebastian <Sebastian@Sebastian-PC>
2016-10-27 21:50:31 -0500

    BOTienda

--- videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOTienda.java
+++ videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOTienda.java
@@ -1,5 +1,16 @@
 package co.edu.eam.ingesoft.videotienda.logica.bos;
 
-public class BOTienda {
+import org.springframework.stereotype.Component;
+
+import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Store;
+
+/**
+ * Clase responsable de la logica de la tienda
+ * @author Jhoan Sebastian Salazar Henao<br/>
+ *         email: jsebastian48@hotmail.com <br/>
+ *         Fecha: 27/10/2016<br/>
+ */
+@Component
+public class BOTienda extends BOGenerico<Store>{
 
 }
