eee9208540fbc89e80eadf972cb2cb7be9ea5a96
GAR-T <GAR-T@GART>
2016-10-28 07:53:14 -0500

    Agregar @component

--- videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOEmpleado.java
+++ videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOEmpleado.java
@@ -2,9 +2,12 @@ package co.edu.eam.ingesoft.videotienda.logica.bos;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import co.edu.eam.ingesoft.videotienda.persistencia.dao.ConstantesNamedQueries;
 import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Staff;
 
+@Component
 public class BOEmpleado extends BOGenerico<Staff>{
 
 	/**
