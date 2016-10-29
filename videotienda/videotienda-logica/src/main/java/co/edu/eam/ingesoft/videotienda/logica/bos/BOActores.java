49e3ce74b8acd6a63d1587ff206105ab6e95a8b2
GAR-T <GAR-T@GART>
2016-10-29 13:05:59 -0500

    metod crear de actor

--- videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOActores.java
+++ videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOActores.java
@@ -5,6 +5,8 @@ package co.edu.eam.ingesoft.videotienda.logica.bos;
 
 import java.util.List;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.eam.ingesoft.videotienda.persistencia.dao.ConstantesNamedQueries;
 import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Actor;
@@ -18,6 +20,7 @@ import co.edu.eam.ingesoft.videotienda.persistencia.entidades.City;
 public class BOActores extends BOGenerico<Actor> {
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void crear(Actor entidad) {
 	
 		Actor actor = buscar(entidad.getActorId());
