1076f53a2a5e487fc7e8ea77851a616e22b4780c
david <david@acer>
2016-11-01 07:43:35 -0500

    cambios

--- videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOReportes.java
+++ videotienda/videotienda-logica/src/main/java/co/edu/eam/ingesoft/videotienda/logica/bos/BOReportes.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import co.edu.eam.ingesoft.videotienda.persistencia.dao.ConstantesNamedQueries;
+import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Rental;
+import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Sale;
 import co.edu.eam.ingesoft.videotienda.persistencia.entidades.Store;
 
 @Component
@@ -19,5 +21,21 @@ public class BOReportes extends BOGenerico<Store>{
 		return dao.ejecutarNamedQuery(ConstantesNamedQueries.CONSULTA_LISTAR_TIENDAS);
 	}
 	
+	/**
+	 * consulta que trae una lista con los datos de la tabla renta de la ventana reportes
+	 * @param s la tienda a la que se le van a buscar los datos
+	 * @return la lista
+	 */
+	public List<Rental>listarTablaRental(Store s){
+		return dao.ejecutarNamedQuery(ConstantesNamedQueries.CONSULTA_LISTAR_DATOS_RENTA,s);
+	}
 	
+	/**
+	 * consulta que trae una lista con los datos de la tabla venta de la ventana reportes
+	 * @param s la tienda a la que se le van a buscar los datos
+	 * @return la lista
+	 */
+	public List<Sale>listarTablaVenta(Store s){
+		return dao.ejecutarNamedQuery(ConstantesNamedQueries.CONSULTA_LISTAR_DATOS_VENTA, s);
+	}
 }
--- videotienda/videotienda-persistencia/src/main/java/co/edu/eam/ingesoft/videotienda/persistencia/dao/ConstantesNamedQueries.java
+++ videotienda/videotienda-persistencia/src/main/java/co/edu/eam/ingesoft/videotienda/persistencia/dao/ConstantesNamedQueries.java
@@ -5,29 +5,34 @@ public class ConstantesNamedQueries {
 	/**
 	 * consulta para listar todos los paises
 	 */
-	public static final String CONSULTA_LISTARPAISES="Pais.listartodos";
-	
-	public static final String CONSULTA_LISTARCIUDADES="Ciudad.ListarTodos";
-	
-	public static final String CONSULTA_LISTAREMPLEADOS="Empleado.ListarTodos";
-	
+	public static final String CONSULTA_LISTARPAISES = "Pais.listartodos";
+
+	public static final String CONSULTA_LISTARCIUDADES = "Ciudad.ListarTodos";
+
+	public static final String CONSULTA_LISTAREMPLEADOS = "Empleado.ListarTodos";
+
 	/**
-<<<<<<< HEAD
-	 * consulta para listar datos de las peliculas por nombre de la pelicula
-=======
-	 * consulta para listar datos de las peliculas..
->>>>>>> branch 'master' of https://github.com/caferrerb/videotiendaDlloeam4.git
+	 * <<<<<<< HEAD consulta para listar datos de las peliculas por nombre de la
+	 * pelicula ======= consulta para listar datos de las peliculas.. >>>>>>>
+	 * branch 'master' of https://github.com/caferrerb/videotiendaDlloeam4.git
 	 */
 	public static final String CONSULTA_POR_NOMBRE_PELICULA = "Film.listarNombrePelicula";
-	
+
 	public static final String CONSULTA_LISTAR_AUTORES = "Actor.ListarTodos";
-	
 
 	public static final String CONSULTA_LISTAR_LENGUAJES = "Language.ListarTodos";
 
 	public static final String CONSULTA_LISTAR_TIENDAS = "Tienda.ListarTodos";
-	
+
 	public static final String CONSULTA_LISTAR_EMPLEADOS_TIENDA = "EmpleadosTienda.ListarTodos";
 
-	
+	/**
+	 * consulta que lista los datos de la tabla rentas en la ventana reporte
+	 */
+	public static final String CONSULTA_LISTAR_DATOS_RENTA = "datosTablaRental.Listar";
+
+	/**
+	 * consulta que lista los datos de la tabla ventas en la ventana reporte
+	 */
+	public static final String CONSULTA_LISTAR_DATOS_VENTA = "datosTablaVentas.Listar";
 }
