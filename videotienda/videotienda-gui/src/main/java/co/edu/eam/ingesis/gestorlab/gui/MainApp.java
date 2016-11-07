e397c9d96dc2062bc04ac8fb7cc02f3451fff589
CAMILO <CAMILO@CAMILO>
2016-11-06 19:31:13 -0500

    Main "Size" ajustado

--- videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesis/gestorlab/gui/MainApp.java
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesis/gestorlab/gui/MainApp.java
@@ -28,7 +28,7 @@ public class MainApp extends Application {
 		FXMLLoader loader = new FXMLLoader();
 		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
-		Scene scene = new Scene(rootNode, 1366, 690);
+		Scene scene = new Scene(rootNode, 1136, 690);
 		scene.getStylesheets().add("/styles/styles.css");
 		// setUserAgentStylesheet(STYLESHEET_MODENA);
 
