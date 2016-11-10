bd7d26a5b6bdd01cb1990a282b06aa098a0397a6
LuchoBolivar <LuchoBolivar@192.168.0.13>
2016-11-10 18:30:50 -0500

    commit 20

--- videotienda/videotienda-persistencia/src/main/java/co/edu/uniquindio/videotienda/dtos/PrestamoDTO.java
+++ videotienda/videotienda-persistencia/src/main/java/co/edu/uniquindio/videotienda/dtos/PrestamoDTO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package co.edu.uniquindio.videotienda.dtos;
 
 import java.io.Serializable;
@@ -72,90 +72,4 @@ public class PrestamoDTO implements Serializable {
 	public void setNombreTienda(String nombreTienda) {
 		this.nombreTienda = nombreTienda;
 	}
-	
-	
-	
-	
-
 }
-=======
-package co.edu.uniquindio.videotienda.dtos;
-
-import java.io.Serializable;
-
-public class PrestamoDTO implements Serializable {
-	
-	private String titulo;
-	private String nombreTienda;
-	private int idPrestamos;
-	
-	public PrestamoDTO() {
-	}
-	
-	
-
-	/**
-	 * @param titulo
-	 * @param nombreTienda
-	 */
-	public PrestamoDTO(String nombreTienda, String titulo,int idpre) {
-		super();
-		this.titulo = titulo;
-		this.nombreTienda = nombreTienda;
-		this.idPrestamos=idpre;
-	}
-
-
-
-	/**
-	 * @return the idPrestamos
-	 */
-	public int getIdPrestamos() {
-		return idPrestamos;
-	}
-
-
-
-	/**
-	 * @param idPrestamos the idPrestamos to set
-	 */
-	public void setIdPrestamos(int idPrestamos) {
-		this.idPrestamos = idPrestamos;
-	}
-
-
-
-	/**
-	 * @return the titulo
-	 */
-	public String getTitulo() {
-		return titulo;
-	}
-
-	/**
-	 * @param titulo the titulo to set
-	 */
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	/**
-	 * @return the nombreTienda
-	 */
-	public String getNombreTienda() {
-		return nombreTienda;
-	}
-
-	/**
-	 * @param nombreTienda the nombreTienda to set
-	 */
-	public void setNombreTienda(String nombreTienda) {
-		this.nombreTienda = nombreTienda;
-	}
-	
-	
-	
-	
-
-}
->>>>>>> branch 'master' of https://github.com/caferrerb/videotiendaDlloeam4.git
