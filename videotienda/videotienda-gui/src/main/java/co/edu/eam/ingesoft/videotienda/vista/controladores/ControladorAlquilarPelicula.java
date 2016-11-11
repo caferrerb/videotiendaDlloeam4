f690f696d65033bc57e84e971e67636e64636735
LuchoBolivar <LuchoBolivar@192.168.0.13>
2016-11-10 22:02:11 -0500

    commit 26

--- videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorAlquilarPelicula.java
+++ videotienda/videotienda-gui/src/main/java/co/edu/eam/ingesoft/videotienda/vista/controladores/ControladorAlquilarPelicula.java
@@ -144,6 +144,7 @@ public class ControladorAlquilarPelicula extends BaseController implements Initi
 				PhFoto.setImage(img);
 				listarPrestamosClientes();
 				jBPrestamo.setDisable(false);
+				jBBuscar.setDisable(true);
 
 			} else {
 				notificar("Busqueda", "El cliente que busca no ha sido encontrado", TipoNotificacion.ERROR);
@@ -179,9 +180,7 @@ public class ControladorAlquilarPelicula extends BaseController implements Initi
 
 				notificar("Prestamo", "Se ha prestado la pelicula", TipoNotificacion.INFO);
 				listarPrestamosClientes();
-				jBPrestamo.setDisable(true);
 				jBBorrar.setDisable(false);
-				jBBuscar.setDisable(true);
 			} catch (ExcepcionNegocio e) {
 				notificar("Prestamo", e.getMessage(), TipoNotificacion.ERROR);
 			}
